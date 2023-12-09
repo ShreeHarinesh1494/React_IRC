c6674300efaf4554b09fc33beecefa68239863e3
Shree Harinesh S <shreeharineshs@gmail.com>
2023-12-09 16:13:13 +0530

    Test

---
+++ Day2/sample/src/App.js
@@ -0,0 +1,20 @@
+import './Assets/css/style.css'
+
+function App()
+{
+    return(
+        <div className="sharon">
+        <form className="hari" >
+                <label>Name</label>
+                <input type="text" name="" id="name"  placeholder="UserName"/>
+                <label>Password</label>
+                <input type="password" name="" id="pass" placeholder="Password"/>
+                <button>Submit</button>
+            
+        </form>
+    </div>
+    
+    )
+}
+
+export default App;
