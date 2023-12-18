cccc142251c72fb3fab49328e9ce12bd025e1417
Shree Harinesh S <shreeharineshs@gmail.com>
2023-12-18 20:47:23 +0530

    home page update but not added

---
+++ cc1/project/src/Components/Content.jsx
@@ -0,0 +1,28 @@
+import '../Assests/css/Content.css'
+const Content = () => {
+  return (
+    <div className='con'>
+      <h1>Tamil Nadu</h1>
+      <p>
+        Tamil Naduis the
+        southernmost state of India. The tenth largest Indian state by area and
+        the sixth largest by population, Tamil Nadu is the home of the Tamil
+        people, whose Tamil language—one of the longest surviving classical
+        languages in the world—is widely spoken in the state and serves as its
+        official language. The capital and largest city is Chennai. Located on
+        the south-eastern coast of the Indian peninsula, Tamil Nadu is defined
+        by the lush Western Ghats and the semi-arid Deccan Plateau in the west,
+        the discontinuous Eastern Ghats in the north, the fertile Eastern
+        Coastal Plains lining the Bay of Bengal in the east, the Gulf of Mannar
+        and the Palk Strait to the south-east, the Laccadive Sea at the southern
+        cape of the peninsula—Kanyakumari, and the river Kaveri bisecting the
+        state. Politically, Tamil Nadu is bound by the Indian states of Kerala,
+        Karnataka, and Andhra Pradesh, and the union territory of Puducherry, as
+        well as an international maritime border with the Northern Province of
+        Sri Lanka at Pamban Island.
+      </p>
+    </div>
+  );
+};
+
+export default Content
